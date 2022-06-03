28208c14de1fa4031c1165764451cba58a4dba65
hunterxx0 <azouzmhamed@gmail.com>
2022-06-03 11:51:00 +0100

    first

---
+++ 0x04-React_component/test/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,59 @@
+import React, { Fragment, Component } from 'react';
+import './Notifications.css';
+import { getLatestNotification } from '../utils/utils';
+import close_icon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+import NotificationItemShape from './NotificationItemShape';
+import PropTypes from 'prop-types';
+
+class Notifications extends Component  {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const props = this.props;
+
+    return (
+      <>
+        <div className='menuItem'>
+          <p>Your notifications</p>
+        </div>
+        { !props.displayDrawer ? null : (
+          <div className='Notifications'>
+          { !props.listNotifications || !props.listNotifications.length ? (<p>No new notifications for now</p>) : (
+            <React.Fragment>
+              <p>Here is the list of notifications</p>
+              <ul>
+                {
+                  props.listNotifications.map(function (notif) {
+                    return (
+                      <NotificationItem key={notif.id} type={notif.type} value={notif.value} html={notif.html} />
+                      );
+                  })
+                }
+              </ul>
+            </React.Fragment>
+            )
+          }
+          <button className='close-b' aria-label='Close' onClick={() => console.log('Close button has been clicked')}>
+              <img src={close_icon} style={{width:'20px', height: '20px'}}/>
+            </button>
+          </div> )
+        }
+      </>
+    );
+  }
+}
+
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+};
+
+export default Notifications;
