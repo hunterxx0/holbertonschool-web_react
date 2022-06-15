972c15311e33d23dc5df14f45707ea22851c1dfd
hunterxx0 <azouzmhamed@gmail.com>
2022-06-15 14:05:46 +0100

    first

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,5 +1,6 @@
 const { fromJS } = require('immutable');
 
+
 export default function getImmutableObject(object) {
 	return fromJS(object);
 }
--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,5 +1,5 @@
 const { Map } = require('immutable');
 
 export default function accessImmutableObject(object, array) {
-  	return fromJS(object).getIn(array, undefined);
+	return fromJS(object).getIn(array, undefined);
 }
