d737bcf87d6e2aa9cb8d4a71fbaff1c24c573b4e
hunterxx0 <azouzmhamed@gmail.com>
2022-06-15 14:47:19 +0100

    first

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,11 +1,6 @@
 import {fromJS} from 'immutable';
 
 
-/**
- * Converting into an Immutable object using fromJS
- * @param {object} object The first object.
- * @return {immutable} fromJS.
- */
 export default function getImmutableObject(object) {
   return fromJS(object);
 }
